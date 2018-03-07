0d70e3bc9367e5f61a0eb2aa62baee7ce3215bac
rjamag <rjamagalhaes@hotmail.com>
2018-03-07 07:42:28 -0300

    app moved to src/components folder

--- mobile/srp/index.js
+++ mobile/srp/index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from "react-native";
+import App from "./src/components/App";
 
-AppRegistry.registerComponent('srp', () => App);
+AppRegistry.registerComponent("srp", () => App);
