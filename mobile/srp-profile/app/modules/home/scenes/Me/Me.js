0ef413d7e883afe6726ea4ea8735f0366ce58a07
rjamag <rjamagalhaes@hotmail.com>
2018-03-22 04:46:01 -0300

    new design

--- mobile/srp-profile/app/modules/home/scenes/Me/Me.js
+++ mobile/srp-profile/app/modules/home/scenes/Me/Me.js
@@ -1,5 +1,30 @@
-import React from "react";
-import { View, StyleSheet, Alert } from "react-native";
+import React, { Component } from "react";
+
+import {
+  View,
+  StyleSheet,
+  Image,
+  Dimensions,
+  FlatList,
+  ScrollView
+} from "react-native";
+
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Left,
+  Body,
+  Right,
+  Thumbnail,
+  Text,
+  Icon,
+  Switch,
+  Button,
+  Separator
+} from "native-base";
 
 import { Actions } from "react-native-router-flux";
 import { connect } from "react-redux";
@@ -11,8 +36,9 @@ import { authReducer } from "../../../auth/index";
 import { actions as userprof } from "../../../home/index";
 import { homeReducer } from "../../../home/index";
 
-import { ScrollView } from "react-native";
-import { Tile, List, ListItem, Button } from "react-native-elements";
+import EntypoIcon from "react-native-vector-icons/Entypo";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+var { height, width } = Dimensions.get("window");
 
 const { signOut } = auth;
 
@@ -20,7 +46,7 @@ const { color } = theme;
 
 const { getCurrentUserProfile } = userprof;
 
-class Me extends React.Component {
+class Services extends Component {
   constructor(props) {
     super(props);
 
@@ -108,60 +134,282 @@ class Me extends React.Component {
   }
 
   render() {
-    //if (this.state.isLoading) return <View />;
-
     if (!this.props) return <View />;
 
     return (
-      <ScrollView style="styles.container">
-        <Tile
-          imageSrc={
-            this.props.user.photoLarge
-              ? { uri: this.props.user.photoLarge }
-              : ""
-          }
-          featured
-          title={
-            this.props.user.facebookDisplayName
-              ? this.props.user.facebookDisplayName.toLowerCase()
-              : ""
-          }
-          caption={this.props.user.email ? this.props.user.email : ""}
-        />
+      <Container style={styles.container}>
+        <Content>
+          <View style={{ paddingTop: 10 }}>
+            {/** User Photo Stats**/}
+            <View style={{ flexDirection: "row" }}>
+              {/**User photo takes 1/3rd of view horizontally **/}
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "flex-start"
+                }}
+              >
+                <Image
+                  source={
+                    this.props.user.photoLarge
+                      ? { uri: this.props.user.photoLarge }
+                      : ""
+                  }
+                  style={{ width: 75, height: 75, borderRadius: 37.5 }}
+                />
+              </View>
 
-        <Button
-          title="sign out"
-          borderRadius={4}
-          buttonStyle={{ marginTop: 20 }}
-          onPress={this.onSignOut}
-        />
+              {/**User Stats take 2/3rd of view horizontally **/}
+              <View style={{ flex: 3 }}>
+                {/** Stats **/}
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-around",
+                    alignItems: "flex-end"
+                  }}
+                >
+                  <View style={{ alignItems: "center" }}>
+                    <Text>20</Text>
+                    <Text style={{ fontSize: 10, color: "grey" }}>Clients</Text>
+                  </View>
+                  <View style={{ alignItems: "center" }}>
+                    <Text>205</Text>
+                    <Text style={{ fontSize: 10, color: "grey" }}>Orders</Text>
+                  </View>
+                  <View style={{ alignItems: "center" }}>
+                    <Text>167</Text>
+                    <Text style={{ fontSize: 10, color: "grey" }}>Reviews</Text>
+                  </View>
+                </View>
 
-        <List>
-          <ListItem
-            title="username"
-            rightTitle={"@" + this.props.user.username}
-            hideChevron
-          />
-        </List>
+                {/**Edit profile and Settings Buttons **/}
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "flex-start",
+                    paddingTop: 10
+                  }}
+                >
+                  <View style={{ flexDirection: "row" }}>
+                    {/** Edit profile takes up 3/4th **/}
+                    <Button
+                      bordered
+                      dark
+                      style={{
+                        flex: 2.5,
+                        marginLeft: 10,
+                        justifyContent: "center",
+                        height: 30
+                      }}
+                    >
+                      <Icon
+                        name="ios-settings"
+                        style={{ height: 30, color: "black" }}
+                      />
+                    </Button>
 
-        <List>
-          <ListItem
-            title="phone"
-            rightTitle={this.props.user.phoneNumber}
-            hideChevron
-          />
-          <ListItem title="address" rightTitle="" onPress={this.editLocation} />
-          <ListItem title="payment" rightTitle="" />
-        </List>
+                    {/** Settings takes up  1/4th place **/}
+                    <Button
+                      bordered
+                      dark
+                      style={{
+                        flex: 1.5,
+                        height: 30,
+                        marginRight: 10,
+                        marginLeft: 5,
+                        justifyContent: "center"
+                      }}
+                    >
+                      <Icon
+                        name="ios-log-out"
+                        style={{ height: 30, color: "black" }}
+                      />
+                    </Button>
+                  </View>
+                </View>
+                {/**End edit profile**/}
+              </View>
+            </View>
 
-        <List>
-          <ListItem title="tutorials" rightTitle="" hideChevron />
-          <ListItem title="support" rightTitle="" hideChevron />
-          <ListItem title="feedback" rightTitle="" hideChevron />
-          <ListItem title="terms of service" rightTitle="" />
-          <ListItem title="work with us" rightTitle="" hideChevron />
-        </List>
-      </ScrollView>
+            <View style={{ paddingBottom: 10 }}>
+              <View style={{ paddingHorizontal: 10 }}>
+                <Text style={{ fontWeight: "500" }}>
+                  {this.props.user.facebookDisplayName
+                    ? this.props.user.facebookDisplayName.toLowerCase()
+                    : ""}
+                </Text>
+                <Text style={{ fontWeight: "300" }}>software engineer</Text>
+                <View>
+                  <View style={{ flex: 1 }}>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "flex-start",
+                        alignItems: "flex-end",
+                        paddingHorizontal: 0
+                      }}
+                    >
+                      <Icon
+                        name="ios-star"
+                        style={{ fontSize: 20, color: "#f2b01e" }}
+                      />
+                      <Icon
+                        name="ios-star"
+                        style={{ fontSize: 20, color: "#f2b01e" }}
+                      />
+                      <Icon
+                        name="ios-star"
+                        style={{ fontSize: 20, color: "#f2b01e" }}
+                      />
+                      <Icon
+                        name="ios-star-half"
+                        style={{ fontSize: 20, color: "#f2b01e" }}
+                      />
+                      <Icon
+                        name="ios-star-outline"
+                        style={{ fontSize: 20, color: "#f2b01e" }}
+                      />
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+
+          <View>
+            <View
+              style={{
+                //flexDirection: "row",
+                // justifyContent: "space-around",
+                borderTopWidth: 1,
+                borderTopColor: "#eae5e5"
+              }}
+            >
+              <List>
+                <Separator
+                  style={{
+                    height: 20,
+                    width: "86%",
+                    backgroundColor: "#fff",
+                    marginLeft: "14%"
+                  }}
+                />
+                <ListItem icon>
+                  <Left>
+                    <Icon name="md-person" />
+                  </Left>
+                  <Body>
+                    <Text>user</Text>
+                  </Body>
+                  <Right>
+                    <Text>{"@" + this.props.user.username}</Text>
+                  </Right>
+                </ListItem>
+                <ListItem icon>
+                  <Left>
+                    <Icon name="ios-mail" />
+                  </Left>
+                  <Body>
+                    <Text>e-mail</Text>
+                  </Body>
+                  <Right>
+                    <Text>
+                      {this.props.user.email ? this.props.user.email : ""}
+                    </Text>
+                  </Right>
+                </ListItem>
+                <Separator
+                  style={{
+                    height: 20,
+                    width: "86%",
+                    backgroundColor: "#fff",
+                    marginLeft: "14%"
+                  }}
+                />
+                <ListItem icon>
+                  <Left>
+                    <Icon name="ios-call" />
+                  </Left>
+                  <Body>
+                    <Text>phone</Text>
+                  </Body>
+                  <Right>
+                    <Text>{this.props.user.phoneNumber}</Text>
+                    <Icon name="arrow-forward" />
+                  </Right>
+                </ListItem>
+                <ListItem icon onPress={this.editLocation}>
+                  <Left>
+                    <Icon name="ios-pin" />
+                  </Left>
+                  <Body>
+                    <Text>address</Text>
+                  </Body>
+                  <Right>
+                    <Text>
+                      {this.props.user.location
+                        ? this.props.user.location.city
+                        : ""}
+                    </Text>
+                    <Icon name="arrow-forward" />
+                  </Right>
+                </ListItem>
+                <ListItem icon>
+                  <Left>
+                    <Icon name="ios-card" />
+                  </Left>
+                  <Body>
+                    <Text>payment</Text>
+                  </Body>
+                  <Right>
+                    <Text>visa</Text>
+                    <Icon name="arrow-forward" />
+                  </Right>
+                </ListItem>
+                <Separator
+                  style={{
+                    height: 20,
+                    width: "86%",
+                    backgroundColor: "#fff",
+                    marginLeft: "14%"
+                  }}
+                />
+                <ListItem icon>
+                  <Left>
+                    <Icon name="ios-help-buoy" />
+                  </Left>
+                  <Body>
+                    <Text>support</Text>
+                  </Body>
+                  <Right />
+                </ListItem>
+                <ListItem icon>
+                  <Left>
+                    <Icon name="ios-information-circle" />
+                  </Left>
+                  <Body>
+                    <Text>feedback</Text>
+                  </Body>
+                  <Right />
+                </ListItem>
+                <ListItem icon>
+                  <Left>
+                    <Icon name="ios-contract" />
+                  </Left>
+                  <Body>
+                    <Text>terms of service</Text>
+                  </Body>
+                  <Right>
+                    <Icon name="arrow-forward" />
+                  </Right>
+                </ListItem>
+              </List>
+            </View>
+          </View>
+        </Content>
+      </Container>
     );
   }
 }
@@ -179,7 +427,11 @@ const mapStateToProps = state => {
 // };
 
 // export default connect(mapStateToProps, mapDispatchToProps)(Me);
-export default connect(mapStateToProps, { getCurrentUserProfile, signOut })(Me);
+export default connect(mapStateToProps, { getCurrentUserProfile, signOut })(
+  Services
+);
+
+//export default Services;
 
 const styles = StyleSheet.create({
   container: {
